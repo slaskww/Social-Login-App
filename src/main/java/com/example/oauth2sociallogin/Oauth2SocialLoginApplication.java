856cce2fef33476a6496ee238e408530de0df23e
lukasz <slaskww@gmail.com>
2020-10-27 23:16:25 +0100

    Add index.html page, add jquery and bootstrap webjars, add oauth2  client registration properties

--- src/main/java/com/example/oauth2sociallogin/Oauth2SocialLoginApplication.java
+++ src/main/java/com/example/oauth2sociallogin/Oauth2SocialLoginApplication.java
@@ -2,9 +2,13 @@ package com.example.oauth2sociallogin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
-public class Oauth2SocialLoginApplication {
+public class Oauth2SocialLoginApplication  {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Oauth2SocialLoginApplication.class, args);
