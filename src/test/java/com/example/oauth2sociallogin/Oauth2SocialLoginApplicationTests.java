114c168c1afcd14ab817dfa9cc628d207de7a538
lukasz <slaskww@gmail.com>
2020-10-27 15:13:30 +0100

    initial commit

---
+++ src/test/java/com/example/oauth2sociallogin/Oauth2SocialLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauth2sociallogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2SocialLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
