2490d8d4d0a79168ddd4caf5e14dc3a286c8766a
lukasz <slaskww@gmail.com>
2020-11-19 20:57:50 +0100

    Add mysql access props, add User entity

---
+++ src/main/java/com/example/oauth2sociallogin/domain/User.java
@@ -0,0 +1,53 @@
+package com.example.oauth2sociallogin.domain;
+
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Arrays;
+import java.util.Collection;
+
+@Entity
+@Data
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue
+     private Long id;
+
+     private String username;
+     private String password;
+     private String oAuth2ProviderId;
+     private String fullName;
+     private String email;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
